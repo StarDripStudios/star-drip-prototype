d12b3bcb514313a54a0d3c0ac888f6fe6d5155ed
Marc <marccrobbins@gmail.com>
2020-04-12 14:13:24 -0400

    added hit point on the planet test

---
+++ Assets/Scripts/PlanetCollisionDetection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlanetCollisionDetection : MonoBehaviour
+{
+    public GameObject hitPointPrefab;
+    public float destroyDelay = 2;
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        foreach (var contact in other.contacts)
+        {
+            print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
+            
+            // Visualize the contact point
+            if (!hitPointPrefab) return;
+            var hitPoint = Instantiate(hitPointPrefab, contact.point,
+                Quaternion.FromToRotation(Vector3.up, contact.normal), transform);
+            Destroy(hitPoint, destroyDelay);
+        }
+    }
+}
