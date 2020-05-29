d43e8f98f67d7295415c325556162598d6e980e7
Marc <marccrobbins@gmail.com>
2020-05-29 00:16:33 -0400

    added color indication of valid

---
+++ Assets/[GAMEASSETS]/Scripts/ColorValidation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ColorValidation : MonoBehaviour
+{
+    public Renderer renderer;
+    public Color validColor;
+
+    private Color originalColor;
+
+    private void Start()
+    {
+        if (!renderer ||
+            !renderer.material) return;
+
+        originalColor = renderer.material.GetColor("_BaseColor");
+    }
+
+    public void ValidateColor()
+    {
+        if (!renderer ||
+            !renderer.material) return;
+        
+        renderer.material.SetColor("_BaseColor", validColor);
+    }
+
+    public void ResetColor()
+    {
+        if (!renderer ||
+            !renderer.material) return;
+        
+        renderer.material.SetColor("_BaseColor", originalColor);
+    }
+}
