d020657505d77b96640d7266526ba58575da136c
Marc <marccrobbins@gmail.com>
2020-06-09 21:38:31 -0400

    basic ass follow

--- Assets/[GAMEASSETS]/Scripts/LevelHeadFollower.cs
+++ Assets/[GAMEASSETS]/Scripts/LevelHeadFollower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,5 +7,31 @@ namespace StarDust
 {
 	public class LevelHeadFollower : MonoBehaviour
 	{
+		public Transform followTransform;
+		public float distance;
+		public float height;
+		public float distanceCheck;
+
+		private Vector3 _followPosition;
+
+		private void LateUpdate()
+		{
+			_followPosition = followTransform.position;
+			_followPosition.z += distance;
+			_followPosition.y += height;
+
+			transform.position = _followPosition;
+		}
+
+		private void OnDrawGizmos()
+		{
+			if (!followTransform) return;
+			
+			Gizmos.color = Color.blue;
+			var position = followTransform.position;
+			position.z += distance;
+			position.y += height;
+			Gizmos.DrawSphere(position, 0.1f);
+		}
 	}
 }
